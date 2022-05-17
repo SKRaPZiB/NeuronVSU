344762cfa8a0d62fa917684569b2030aff7188ab
SKRaPZiB <70914000+SKRaPZiB@users.noreply.github.com>
2022-05-17 22:40:51 +0300

    Test #1

---
+++ number1/main.py
@@ -0,0 +1,21 @@
+from reader import DataReader
+from perceptron import Perceptron
+
+
+NU = 000.1
+H = 0.7
+# learning_rate - это скорость обучения
+
+
+def main():
+    train_data = DataReader(4).get_train_data()
+    perceptron = Perceptron(size=len(train_data[0].data), h=H)
+
+    perceptron.train(train_data=train_data, nu=NU)
+
+    for item in train_data:
+        print(f'Prediction for number {item.number} is:  ', perceptron.predict(item))
+
+
+if __name__ == '__main__':
+    main()
